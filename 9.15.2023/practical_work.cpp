1b80f5f418d5e51cedf73ec047fa592c116b66a4
lazy <144381160+subconstruction@users.noreply.github.com>
2023-09-15 16:20:13 +0300

    Update practical_work.cpp

--- 9.15.2023/practical_work.cpp
+++ 9.15.2023/practical_work.cpp
@@ -13,16 +13,13 @@ int main() {
         for (int j = 1; j <= 10; ++j) {
             int result = i * j;
 
-            if (i == 2 && j == 2) {
-                iter = "2x2=" + to_string(result);
-            }
-
+            if (i == 2 && j == 2) iter = "2x2=" + to_string(result);
             cout << i << " x " << j << " = " << result << "\n";
         }
 
         cout << "\n";
     }
 
-    cout << "My iteration => " << iter;
+    cout << "My iteration: " << iter;
     return 0;
 }
