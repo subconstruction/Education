55f97dd382a368dea024be5091aa3804a2bbe9c8
lazy <144381160+subconstruction@users.noreply.github.com>
2023-09-20 13:21:46 +0300

    xd

--- 20.09.2023/classwork.cpp
+++ 20.09.2023/classwork.cpp
@@ -3,12 +3,24 @@
 
 using namespace std;
 
+int genN(int min, int max) {
+    int i = (rand()%(max-min+1)+min);
+
+    return i;
+}
+
+int doStackOverflow(int a) {
+    return doStackOverflow(a);
+}
+
 int main() 
 { 
-    int i;
+    doStackOverflow(2);
+    srand((unsigned)time(NULL));
 
-    srand((unsigned)time(NULL)); 
-    i = (rand()%6)+1; 
+    for (int i=0;i<=10;i++) {
+        cout << genN(10, 20) << "\n"; 
+    }
 
-    cout << i << "\n"; 
+    return 0;
 }
