05ccd9270e8c1c67851e997366de317816f95455
lazy <144381160+subconstruction@users.noreply.github.com>
2023-09-26 09:35:41 +0300

    Update

---
+++ 26.09.2023/alg.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+uint alg(uint a, uint b) {
+    while (b != 0) {
+        uint last_int = b;
+        b = a % b;
+
+        a = last_int;
+    }
+
+    return a;
+}
+
+int main() {
+    unsigned int a, b, result;
+
+    std::cout << "Введите a,b\n";
+    std::cin >> a >> b;
+
+    result = alg(a, b);
+    std::cout << "НОД -> " << result;
+
+    return 0;
+}
