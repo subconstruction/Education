264be59a057d6ea06a60020621f1ce4bdf74381b
lazy <144381160+subconstruction@users.noreply.github.com>
2023-09-26 09:21:06 +0300

    Update

---
+++ 26.09.2023/quadratic_equation.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+std::vector<std::string> split(const std::string& str, char delimiter) {
+    std::vector<std::string> tokens;
+    std::istringstream tokenStream(str);
+    std::string token;
+
+    while (std::getline(tokenStream, token, delimiter)) {
+        tokens.push_back(token);
+    }
+
+    return tokens;
+}
+
+int main() {
+    std::string input = "1,9,1";
+    double a,b,c,Root, Root2;
+
+    std::cout << "Укажите a,b,c (используйте запятую как разделитель)" << "\n";
+    //std::cin >> input;
+
+    char delimiter = ',';
+    
+    std::vector<std::string> tokens = split(input, delimiter);
+
+    a = std::stod(tokens[0]);
+    b = std::stod(tokens[1]);
+    c = std::stod(tokens[2]);
+
+    double discriminant = (a*a) + (4*a*c);
+
+    if (discriminant < 0) {
+        std::cout << "Discriminant is under 0";
+
+        return 0;
+    }
+
+    Root = (-b + sqrt(discriminant)) / 2*a;
+    Root2 = (-b - sqrt(discriminant)) / 2*a;
+
+    printf("Discriminant -> %d | Root -> %d | Root2 -> %d", discriminant, Root, Root2);
+
+    return 0;
+}
