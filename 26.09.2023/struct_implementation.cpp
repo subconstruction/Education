5a2962535915bc2eedc5aa6277733aed1ca3a7e6
lazy <144381160+subconstruction@users.noreply.github.com>
2023-09-29 13:43:18 +0300

    upd

--- 26.09.2023/struct_implementation.cpp
+++ 26.09.2023/struct_implementation.cpp
@@ -2,22 +2,41 @@
 
 using namespace std;
 
-struct String {
-private:
+char fill_str(size_t size, const char fill_char) {
+    char new_str[size];
+
+    for (int i = 0; i <= size; i++) {
+        new_str[i] = fill_char;
+    }
+
+    return new_str[size];
+}
+
+class String {
     char* memory;
     size_t length;
     size_t capacity;
 
 public:
-    String(const char* str)
-        : memory(char[str.size()])
+    String(size_t l, char c)
+        : memory(new char[l]), length(l), capacity(l)
     {
-        cout >> "rofl";
+        memset(memory, length, capacity);
     }
-}
+
+    String(const String& str) {
+        memory = fill_str(10, '1');
+
+        memcpy(memory, str.memory, length);
+    }
+
+    ~String() {
+        delete[] memory;
+    }
+};
 
 int main() {
-
+    String my_str(2, 'e');
 
     return 0;
 }
