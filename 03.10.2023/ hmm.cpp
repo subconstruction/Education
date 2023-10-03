7c4d2ae456ba865c16e04a140ccedc8c8ba7388b
lazy <144381160+subconstruction@users.noreply.github.com>
2023-10-03 08:49:05 +0300

    zxc

---
+++ 03.10.2023/ hmm.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+int genN(int min, int max) {return rand() % (max - min + 1) + min;};
+
+int main() {
+    vec = std::vector<int>;
+
+    while (true) {
+        uint8_t num = genN(10, 25);
+
+
+    }
+
+    return 0;
+}
