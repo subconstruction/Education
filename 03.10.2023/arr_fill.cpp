5fe5c76008a49dfbd2ece9b2147a9215d094f370
lazy <144381160+subconstruction@users.noreply.github.com>
2023-10-03 09:03:54 +0300

    hhhh

---
+++ 03.10.2023/arr_fill.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <ctime>
+
+int genN(int min, int max) {return rand() % (max - min + 1) + min;}
+const bool searchForElement(const std::vector<int> &vector, int element) {
+    for (int n : vector) {
+        if (n == element) return true;
+    }
+
+    return false;
+}
+
+int main() {
+    srand((unsigned int)time(NULL));
+    
+    std::vector<int> newVec;
+
+    while (true) {
+        if (newVec.size() == 10) break;
+        uint8_t num = genN(10, 25);
+
+        if (searchForElement(newVec, num)) {
+            printf("Skipped [%d] ~ found in vector<int>\n", num);
+            continue;
+        }
+
+        newVec.push_back(num);
+
+        printf("Added [%d] to vector, now vector<int> = { ", num);
+        for (int i : newVec) {
+            printf(" %d", i);
+        }
+
+        std::cout << " }" << std::endl;
+    }
+
+    return 0;
+}
