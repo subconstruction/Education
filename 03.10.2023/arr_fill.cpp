6b1bb8f3d560ca82c7cd4dc5062cc27462b69fa7
lazy <144381160+subconstruction@users.noreply.github.com>
2023-10-03 09:29:37 +0300

    .

--- 03.10.2023/arr_fill.cpp
+++ 03.10.2023/arr_fill.cpp
@@ -2,9 +2,9 @@
 #include <vector>
 #include <ctime>
 
-int genN(int min, int max) {return rand() % (max - min + 1) + min;}
-const bool searchForElement(const std::vector<int> &vector, int element) {
-    for (int n : vector) {
+short genN(short min, short max) {return rand() % (max - min + 1) + min;}
+const bool searchForElement(const std::vector<short> &vector, short element) {
+    for (short n : vector) {
         if (n == element) return true;
     }
 
@@ -12,23 +12,23 @@ const bool searchForElement(const std::vector<int> &vector, int element) {
 }
 
 int main() {
-    srand((unsigned int)time(NULL));
+    srand((unsigned short)time(NULL));
     
-    std::vector<int> newVec;
+    std::vector<short> newVec;
 
     while (true) {
         if (newVec.size() == 10) break;
-        unsigned int num = genN(10, 25);
+        unsigned short num = genN(10, 25);
 
         if (searchForElement(newVec, num)) {
-            printf("Skipped [%d] ~ found in vector<int>\n", num);
+            printf("Skipped [%d] ~ found in vector<short>\n", num);
             continue;
         }
 
         newVec.push_back(num);
 
-        printf("Added [%d] to vector, now vector<int> = { ", num);
-        for (int i : newVec) {
+        printf("Added [%d] to vector, now vector<short> = { ", num);
+        for (short i : newVec) {
             printf(" %d", i);
         }
 
