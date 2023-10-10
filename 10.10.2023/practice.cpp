54f40f1995670b80480401d1faff33f19d49abde
lazy <144381160+subconstruction@users.noreply.github.com>
2023-10-10 08:39:58 +0300

    .

---
+++ 10.10.2023/practice.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+using std::cout;
+
+void shakerSort(std::vector<int>& arr) {
+    bool swapped;
+    size_t n = arr.size();
+    
+    do {
+        swapped = false;
+        
+        for (int i = 0; i < n - 1; ++i) {
+            if (arr[i] > arr[i + 1]) {
+                std::swap(arr[i], arr[i + 1]);
+                swapped = true;
+            }
+        }
+        
+        if (!swapped) {
+            break;
+        }
+
+        for (int i = n - 2; i >= 0; --i) {
+            if (arr[i] > arr[i + 1]) {
+                std::swap(arr[i], arr[i + 1]);
+                swapped = true;
+            }
+        }
+    } while (swapped);
+}
+
+int main() {
+    std::vector<int> arr = {9, 55, 2, 2, 4, 26, 67, 1, 7, 4, 7, 83, 645, 234, 23, 564, 34, 12};
+
+    shakerSort(arr);
+
+    std::cout << "Sorted array: ";
+    
+    for (int num : arr) {
+        std::cout << num << "\t";
+    }
+
+    return 0;
+}
