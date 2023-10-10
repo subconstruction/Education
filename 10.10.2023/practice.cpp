f9fa0cf658988c6e0d4c5a112748ebdf976f3d2e
lazy <144381160+subconstruction@users.noreply.github.com>
2023-10-10 09:51:06 +0300

    /

--- 10.10.2023/practice.cpp
+++ 10.10.2023/practice.cpp
@@ -1,45 +1,76 @@
 #include <iostream>
 #include <vector>
+#include <chrono>
+#include <cstdlib>
+#include <ctime>
 
 using std::cout;
+
+void generateRandomArray(std::vector<int> &arr, int size) {
+    for (int i = 0; i < size; ++i) {
+        arr.push_back(std::rand());
+    }
+}
 
 void shakerSort(std::vector<int>& arr) {
     bool swapped;
     size_t n = arr.size();
-    
+
+    int left = 0;
+    int right = n - 1;
+    int lastSwapped = right;
+
     do {
         swapped = false;
-        
-        for (int i = 0; i < n - 1; ++i) {
+
+        for (int i = left; i < right; ++i) {
             if (arr[i] > arr[i + 1]) {
                 std::swap(arr[i], arr[i + 1]);
                 swapped = true;
+                lastSwapped = i;
             }
         }
-        
+
+        right = lastSwapped;
+
         if (!swapped) {
             break;
         }
 
-        for (int i = n - 2; i >= 0; --i) {
+        swapped = false;
+
+        for (int i = right - 1; i >= left; --i) {
             if (arr[i] > arr[i + 1]) {
                 std::swap(arr[i], arr[i + 1]);
                 swapped = true;
+                lastSwapped = i;
             }
         }
-    } while (true);
+
+        left = lastSwapped + 1;
+
+    } while (swapped);
 }
 
 int main() {
-    std::vector<int> arr = {9, 55, 2, 2, 4, 26, 67, 1, 7, 4, 7, 83, 645, 234, 23, 564, 34, 12};
+    const int arraySize = 100;
+    std::vector<int> arr;
 
+    std::srand(std::time(nullptr));
+    generateRandomArray(arr, arraySize);
+    
+    double start, end;
+    start = clock();
     shakerSort(arr);
+    end = clock();
 
-    cout << "Sorted array: ";
+    std::cout << "Sorted array: ";
     
     for (int num : arr) {
-        cout << num << "\t";
+        std::cout << num << "\t";
     }
+    
+    std::cout << "\n[Sort] Runtime: " << end - start << "ms" << std::endl;
 
     return 0;
 }
