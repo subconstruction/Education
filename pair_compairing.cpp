2eebb78235b94be7fdc6ac379abd6caf5df3e215
lazy <144381160+subconstruction@users.noreply.github.com>
2023-10-31 10:23:12 +0300

    Update pair_compairing.cpp

--- pair_compairing.cpp
+++ pair_compairing.cpp
@@ -7,14 +7,14 @@ using namespace std;
 int main() {
     srand(static_cast<unsigned>(time(0)));
 
-    int array[100];
-    int count = 0;
+    short array[100];
+    short count = 0;
 
-    for (int i = 0; i < 100; ++i) {
+    for (short i = 0; i < 100; ++i) {
         array[i] = rand() % 10001 - 5000;
     }
 
-    for (int i = 0; i < 50; ++i) {
+    for (short i = 0; i < 50; ++i) {
         if ((array[i] + array[99 - i]) % 2 != 0 && (array[i] + array[99 - i]) > 0) {
             count++;
         }
