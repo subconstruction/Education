58d3b1e17d1a01e7e3ffe3d5f72fcc321b20c486
lazy <144381160+subconstruction@users.noreply.github.com>
2023-11-14 09:16:46 +0300

    Update Header.h

--- lab_11-OOP/Header.h
+++ lab_11-OOP/Header.h
@@ -15,7 +15,7 @@ public:
     void loadFromFile(const std::string& filename) {
         std::ifstream file(filename);
         if (!file.is_open()) {
-            std::cerr << "Не удалось открыть файл для чтения." << std::endl;
+            std::cerr << "Файл не найден" << std::endl;
             return;
         }
 
