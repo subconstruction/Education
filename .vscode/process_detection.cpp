ec48fa4146545bccc936329ff5c9d7fdaa847767
lazy <144381160+subconstruction@users.noreply.github.com>
2023-12-19 11:06:36 +0300

    Update process_detection.cpp

--- .vscode/process_detection.cpp
+++ .vscode/process_detection.cpp
@@ -3,6 +3,52 @@
 #include <windows.h>
 #include <tlhelp32.h>
 
+bool UnmuteVolume() {
+    HRESULT hr;
+    IMMDeviceEnumerator* pEnumerator = nullptr;
+    IMMDevice* pDevice = nullptr;
+    IAudioEndpointVolume* pVolume = nullptr;
+
+    // Initialize COM library
+    CoInitialize(nullptr);
+
+    // Create the device enumerator
+    hr = CoCreateInstance(__uuidof(MMDeviceEnumerator), nullptr, CLSCTX_ALL, __uuidof(IMMDeviceEnumerator), (void**)&pEnumerator);
+
+    if (SUCCEEDED(hr)) {
+        // Get the default audio endpoint
+        hr = pEnumerator->GetDefaultAudioEndpoint(eRender, eConsole, &pDevice);
+    }
+
+    if (SUCCEEDED(hr)) {
+        // Activate the audio endpoint volume interface
+        hr = pDevice->Activate(__uuidof(IAudioEndpointVolume), CLSCTX_ALL, nullptr, (void**)&pVolume);
+    }
+
+    if (SUCCEEDED(hr)) {
+        // Unmute the volume
+        hr = pVolume->SetMute(FALSE, nullptr);
+    }
+
+    // Release COM objects
+    if (pVolume) {
+        pVolume->Release();
+    }
+
+    if (pDevice) {
+        pDevice->Release();
+    }
+
+    if (pEnumerator) {
+        pEnumerator->Release();
+    }
+
+    // Uninitialize COM library
+    CoUninitialize();
+
+    return SUCCEEDED(hr);
+}
+
 // Function to check if a process with the specified name is running
 bool IsProcessRunning(const std::wstring& processName) {
     HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
