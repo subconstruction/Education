01319498cff6b5a2ceacd2621a5023dc1adf616b
lazy <144381160+subconstruction@users.noreply.github.com>
2023-12-19 13:06:36 +0300

    Update play_sound_if_browser_openned.cpp

--- play_sound_if_browser_openned.cpp
+++ play_sound_if_browser_openned.cpp
@@ -159,6 +159,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
                 INT nShowCmd = SW_SHOWNORMAL;
 
                 ShellExecuteA(NULL, lpOperation, "https://yandex.ru/search/?text=%D0%9A%D0%B0%D0%BA%D0%B8%D0%B5+%D0%BF%D0%BE%D1%81%D0%BB%D0%B5%D0%B4%D1%81%D1%82%D0%B2%D0%B8%D1%8F+%D0%B2%D0%B5%D0%B4%D0%B5%D1%82+%D0%B7%D0%B0+%D1%81%D0%BE%D0%B1%D0%BE%D0%B9+%D1%81%D0%BF%D0%B8%D1%81%D1%8B%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5%3F&clid=2411726&lr=213", NULL, lpDirectory, nShowCmd);
+                
+                break;
+
                 std::wcout << processName << " is running." << std::endl;
             }
             else {
