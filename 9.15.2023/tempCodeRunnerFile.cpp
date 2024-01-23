1dc11a6105a62ccdf146782ce8c71aefd146de96
lazy <144381160+subconstruction@users.noreply.github.com>
2024-01-23 10:27:44 +0300

    Update tempCodeRunnerFile.cpp

--- 9.15.2023/tempCodeRunnerFile.cpp
+++ 9.15.2023/tempCodeRunnerFile.cpp
@@ -1,51 +1,88 @@
-#include "stdafx.h"
-#include <string>
+#include <windows.h>
 
-namespace YourAppName {
+// Global variables
+HWND g_hWnd;
+HWND g_hButton;
+HWND g_hText;
 
-    using namespace System;
-    using namespace System::Windows::Forms;
+// Function declarations
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+void CreateControls(HWND hWnd);
+void ToggleTextVisibility();
 
-    public ref class MainForm : public Form {
-    public:
-        MainForm() {
-            InitializeComponents();
-        }
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    // Register the window class.
+    const char CLASS_NAME[] = "Sample Window Class";
 
-    private:
-        void InitializeComponents() {
-            // TextBox
-            textBox = gcnew TextBox();
-            textBox->Multiline = true;
-            textBox->Size = Drawing::Size(200, 100);
-            textBox->Location = Drawing::Point(10, 10);
-            this->Controls->Add(textBox);
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
 
-            // Button
-            button = gcnew Button();
-            button->Text = "Toggle Text";
-            button->Size = Drawing::Size(100, 30);
-            button->Location = Drawing::Point(10, 120);
-            button->Click += gcnew EventHandler(this, &MainForm::ToggleText);
-            this->Controls->Add(button);
-        }
+    RegisterClass(&wc);
 
-        void ToggleText(Object^ sender, EventArgs^ e) {
-            // Toggle visibility of the text
-            textBox->Visible = !textBox->Visible;
-        }
+    // Create the window.
+    g_hWnd = CreateWindowEx(
+        0,                              // Optional window styles.
+        CLASS_NAME,                     // Window class
+        "Windows App with Text and Button", // Window text
+        WS_OVERLAPPEDWINDOW,            // Window style
 
-    private:
-        TextBox^ textBox;
-        Button^ button;
-    };
+        // Size and position
+        CW_USEDEFAULT, CW_USEDEFAULT, 500, 300,
+
+        NULL,       // Parent window
+        NULL,       // Menu
+        hInstance,  // Instance handle
+        NULL        // Additional application data
+    );
+
+    if (g_hWnd == NULL) {
+        return 0;
+    }
+
+    ShowWindow(g_hWnd, nCmdShow);
+
+    // Run the message loop.
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return 0;
 }
 
-[STAThread]
-int main(array<System::String ^> ^args) {
-    Application::EnableVisualStyles();
-    Application::SetCompatibleTextRenderingDefault(false);
-    YourAppName::MainForm form;
-    Application::Run(%form);
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    switch (message) {
+    case WM_CREATE:
+        CreateControls(hWnd);
+        break;
+    case WM_COMMAND:
+        if (lParam == (LPARAM)g_hButton) {
+            ToggleTextVisibility();
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
     return 0;
+}
+
+void CreateControls(HWND hWnd) {
+    // Create a button
+    g_hButton = CreateWindowEx(0, "BUTTON", "Toggle Text", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+        10, 10, 100, 30, hWnd, NULL, NULL, NULL);
+
+    // Create a static text control
+    g_hText = CreateWindowEx(0, "STATIC", "Desired Text", WS_VISIBLE | WS_CHILD | SS_LEFT,
+        10, 50, 200, 100, hWnd, NULL, NULL, NULL);
+}
+
+void ToggleTextVisibility() {
+    // Toggle visibility of the text control
+    ShowWindow(g_hText, IsWindowVisible(g_hText) ? SW_HIDE : SW_SHOW);
 }
